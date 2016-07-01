788e8a4054576db569e43a9e7b544ad9a046c801
Binhbt <binhbt@vega.com.vn>
2016-07-01 10:36:34 +0700

    update

--- VegaRecyclerView-sample/src/main/java/com/malinskiy/superrecyclerview/sample/sectioned/Example1Fragment.java
+++ VegaRecyclerView-sample/src/main/java/com/malinskiy/superrecyclerview/sample/sectioned/Example1Fragment.java
@@ -46,7 +46,6 @@ public class Example1Fragment extends Fragment  implements SwipeRefreshLayout.On
         }
 
         RecyclerViewWrapper recyclerView = (RecyclerViewWrapper) view.findViewById(R.id.list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(sectionAdapter);
         recyclerView.setRefreshListener(this);
         recyclerView.setRefreshingColorResources(android.R.color.holo_orange_light, android.R.color.holo_blue_light, android.R.color.holo_green_light, android.R.color.holo_red_light);
